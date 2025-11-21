9d8fea37d23565b686596f9610afbe67b898fa1d
makramamor-devoteam <makram.amor@devoteam.com>
2025-11-21 14:15:56 +0100

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,13 @@
 import multiprocessing
+import os
 
 max_requests = 1000
 max_requests_jitter = 50
 log_file = "-"
-bind = "0.0.0.0"
+
+# Azure provides PORT env variable
+bind = f"0.0.0.0:{os.environ.get('PORT', 8000)}"
+
 num_cpus = multiprocessing.cpu_count()
 workers = (num_cpus * 2) + 1
 threads = 1 if num_cpus == 1 else 2
